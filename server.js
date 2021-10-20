044553f94f49b0da743da20da1a09c3e7d525b3c
Daniel O <danielolvera2188@gmail.com>
2021-10-19 23:52:46 -0600

    added logic to models

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+const sequelize = require('./config/connection')
+const routes = require('./controllers');
+// const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 const session = require('express-session');
@@ -22,13 +24,13 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(routes);
+// // turn on routes
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(routes);
 
-//setup for express-handlebars
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+// //setup for express-handlebars
+// app.engine('handlebars', hbs.engine);
+// app.set('view engine', 'handlebars');
 
 //turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
